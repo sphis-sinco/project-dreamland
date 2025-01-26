63098b4a07dbcc9b5fcea2277913b508fc916929
IdealistCat <sinconsistencia@gmail.com>
2025-01-25 22:58:12 -0600

    checking if we need to update aka open source is awsome

--- source/Main.hx
+++ source/Main.hx
@@ -6,12 +6,36 @@ import haxe.Json;
 import lime.app.Application;
 import openfl.display.Sprite;
 
+using StringTools;
+
 class Main extends Sprite
 {
 	public function new()
 	{
 		FlxG.save.bind('dreamland', Application.current.meta.get('company'));
+		trace('checking for update');
+		var http = new haxe.Http("https://raw.githubusercontent.com/sphis-Sinco/project-dreamland/refs/heads/master/version.txt");
+
+		http.onData = function(data:String)
+		{
+			var updateVersion = data.split('\n')[0].trim();
+			var curVersion:String = Application.current.meta.get('version').trim();
+			trace('version online: ' + updateVersion + ', your version: ' + curVersion);
+			if (updateVersion != curVersion)
+			{
+				trace('versions arent matching!');
+				FlxG.save.data.latest_version = updateVersion;
+			}
+		}
+
+		http.onError = function(error)
+		{
+			trace('error: $error');
+		}
+
+		http.request();
+		
 		super();
-		addChild(new FlxGame(0, 0, (FlxG.save.data.version != Application.current.meta.get('version')) ? OutdatedState : MenuState));
+		addChild(new FlxGame(0, 0, (Application.current.meta.get('version') != FlxG.save.data.latest_version) ? OutdatedState : MenuState));
 	}
 }
