83ddfee27e6babe5cd1c3ea049eacf63edcb5063
IdealistCat <sinconsistencia@gmail.com>
2025-01-25 21:58:15 -0600

    green score text when you get high score but better implemenetation

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -129,8 +129,7 @@ class PlayState extends FlxState
 					if (enemy.overlaps(bullet))
 					{
 						SCORE += 100;
-						Global.set_HIGHSCORE();
-						if (Global.NEW_HIGHSCORE)
+						if (Global.HIGHSCORE < SCORE)
 							score_text.color = FlxColor.LIME;
 
 						enemy.destroy();
