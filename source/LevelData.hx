9287b9bc5f228e5e72b6e1e04cae20fb642c4f98
IdealistCat <sinconsistencia@gmail.com>
2025-01-26 16:22:30 -0600

    defaultJSON variable inside a LevelDataManager

--- source/LevelData.hx
+++ source/LevelData.hx
@@ -43,3 +43,32 @@ typedef LDSSpeeds =
 	var ?enemy_easy:Float;
 	var ?enemy_common:Float;
 }
+class LevelDataManager
+{
+	public static var defaultJSON:LevelData = {
+		"difficulty": "medium",
+		"author": "Sphis_Sinco",
+		"assets": {
+			"directory": "",
+			"enemy_rare": "enemy-rare",
+			"enemy_easy": "enemy-easy",
+			"enemy_common": "enemy-common"
+		},
+		"settings": {
+			"scores": {
+				"enemy_rare": 125,
+				"enemy_easy": 50,
+				"enemy_common": 25
+			},
+			"chances": {
+				"enemy_rare": 10,
+				"enemy_easy": 85
+			},
+			"speed_additions": {
+				"enemy_common": 0,
+				"enemy_easy": -10,
+				"enemy_rare": 10
+			}
+		}
+	}
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import LevelData.LevelDataManager;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -59,33 +60,7 @@ class PlayState extends FlxState
 		catch (e)
 		{
 			// trace(e);
-			level_data = {
-				"difficulty": "medium",
-				"author": "Sphis_Sinco",
-				"assets": {
-					"directory": "",
-
-					"enemy_rare": "enemy-rare",
-					"enemy_easy": "enemy-easy",
-					"enemy_common": "enemy-common"
-				},
-				"settings": {
-					"scores": {
-						"enemy_rare": 125,
-						"enemy_easy": 50,
-						"enemy_common": 25
-					},
-					"chances": {
-						"enemy_rare": 10,
-						"enemy_easy": 85
-					},
-					"speed_additions": {
-						"enemy_common": 0,
-						"enemy_easy": -10,
-						"enemy_rare": 10
-					}
-				}
-			}
+			level_data = LevelDataManager.defaultJSON;
 		}
 
 		super.create();
