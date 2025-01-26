437c4938331f84a093240c3fc63b9e114a70ea0d
IdealistCat <sinconsistencia@gmail.com>
2025-01-25 19:53:05 -0600

    green square for our player

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,30 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var player:Player = new Player();
+
 	override public function create()
 	{
+		player.screenCenter();
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+	}
+}
+
+class Player extends FlxSprite
+{
+	override public function new(X:Float = 0, Y:Float = 0)
+	{
+		super(X,Y);
+		makeGraphic(32,32, 0x00ff00);
 	}
 }
