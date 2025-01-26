c36cf2580247a4d1c07d13bea46608ec54344377
IdealistCat <sinconsistencia@gmail.com>
2025-01-25 23:26:17 -0600

    read a levels array and display text to the screen

--- source/LevelSelect.hx
+++ source/LevelSelect.hx
@@ -1,11 +1,27 @@
 package;
 
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
 
 class LevelSelect extends FlxState
 {
+	var levels:Array<String> = ['heaven', 'earth', 'hell'];
+	var level_texts:FlxTypedGroup<FlxText> = new FlxTypedGroup<FlxText>();
+
 	override public function create()
 	{
+		add(level_texts);
+
+		var int:Int = 0;
+		for (level in levels)
+		{
+			var new_text:FlxText = new FlxText(0, 8 + (16 * int), 0, level, 16);
+			level_texts.add(new_text);
+
+			int++;
+		}
+
 		super.create();
 	}
 
