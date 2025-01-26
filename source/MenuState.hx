d052fa64ce9da6bbff7a2aeed427fd0620e4e3b5
IdealistCat <sinconsistencia@gmail.com>
2025-01-25 20:29:25 -0600

    fix overlapping drug text

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,7 +9,7 @@ class MenuState extends FlxState
 	var menuText:FlxText = new FlxText(0, 0, 0, "Main Menu", 16);
 	var pressButton:FlxText = new FlxText(0, 16, 0, "Press enter to play", 16);
 
-	var drugText:FlxText = new FlxText(0, 16, 0, "Highscore: 0", 16);
+	var drugText:FlxText = new FlxText(0, 32, 0, "Highscore: 0", 16);
 
 	override public function create()
 	{
