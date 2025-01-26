c07e2814d89bd5b7b503c44d19615d8fa3edac68
IdealistCat <sinconsistencia@gmail.com>
2025-01-25 19:49:10 -0600

    press enter to play text

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -6,10 +6,12 @@ import flixel.text.FlxText;
 class MenuState extends FlxState
 {
 	var menuText:FlxText = new FlxText(0, 0, 0, "Main Menu", 16);
+	var pressButton:FlxText = new FlxText(0, 0, 0, "Press enter to play", 16);
 
 	override public function create()
 	{
 		add(menuText);
+		add(pressButton);
 
 		super.create();
 	}
