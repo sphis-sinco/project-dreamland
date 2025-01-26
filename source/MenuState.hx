fa98e522f50bffcff4fbb6f0bef98d224fc3e95c
IdealistCat <sinconsistencia@gmail.com>
2025-01-25 19:48:42 -0600

    menu text to the main menu

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,11 +1,16 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class MenuState extends FlxState
 {
+	var menuText:FlxText = new FlxText(0, 0, 0, "Main Menu", 16);
+
 	override public function create()
 	{
+		add(menuText);
+
 		super.create();
 	}
 
