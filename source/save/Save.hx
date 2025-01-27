7f206fef7c4b6621a87c46091d9bcec253e64745
IdealistCat <sinconsistencia@gmail.com>
2025-01-27 06:38:39 -0600

    any null save info should be set

--- source/save/Save.hx
+++ source/save/Save.hx
@@ -6,6 +6,8 @@ class Save extends FlxSave
 {
 	public var savedata:SaveData;
 
+	public var SAVEDATA_VERSION:Int = 2;
+
 	override public function new()
 	{
 		super();
@@ -14,23 +16,29 @@ class Save extends FlxSave
 		if (this.data.savedata != null)
 		{
 			savedata = this.data.savedata;
+			savedata.saveVer = SAVEDATA_VERSION;
+			savedata.firstTime ??= true;
+			savedata.highscore ??= 0;
 		}
 		else
 		{
 			trace('SAVEDATA IS NULL. SETTING TO A COMPLETELY NEW SAVE.');
 			savedata = {
+				saveVer: SAVEDATA_VERSION,
 				firstTime: true,
 				highscore: 0
 			};
-			this.data.savedata = savedata;
 		}
 
+		this.data.savedata = savedata;
 		this.flush();
 	}
 }
 
 typedef SaveData =
 {
+	var saveVer:Int;
+
 	var firstTime:Bool;
 	var highscore:Int;
 }
