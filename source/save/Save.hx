463b464f86d54e6a2eee4e9df8600be886d654ae
IdealistCat <sinconsistencia@gmail.com>
2025-01-27 05:47:56 -0600

    null checks n stuff

--- source/save/Save.hx
+++ source/save/Save.hx
@@ -9,7 +9,23 @@ class Save extends FlxSave
 	override public function new()
 	{
 		super();
+
 		bind('dreamland', Application.current.meta.get('company'));
+		if (this.data.savedata != null)
+		{
+			savedata = this.data.savedata;
+		}
+		else
+		{
+			trace('SAVEDATA IS NULL. SETTING TO A COMPLETELY NEW SAVE.');
+			savedata = {
+				firstTime: true,
+				highscore: 0
+			};
+			this.data.savedata = savedata;
+		}
+
+		this.flush();
 	}
 }
 
