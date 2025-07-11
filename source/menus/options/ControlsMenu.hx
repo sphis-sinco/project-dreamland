cb263cc69e0880fed85a0685dfa2dad156f3c984
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-11 09:23:30 -0500

    chore: control list

--- source/menus/options/ControlsMenu.hx
+++ source/menus/options/ControlsMenu.hx
@@ -4,10 +4,26 @@ import base.TextSelecting;
 
 class ControlsMenu extends TextSelecting
 {
+	public var control_id:Map<String, String> = [];
+
+	public function newControl(name:String, controlId:String)
+	{
+		texts.push(name);
+		control_id.set(name, controlId);
+	}
+
+	override public function new()
+	{
+		super();
+
+		texts = [];
+		newControl('Gameplay Shoot', 'gameplay_shoot');
+		newControl('Gameplay Move Up', 'gameplay_move_up');
+		newControl('Gameplay Move Down', 'gameplay_move_down');
+	}
+
 	override public function create()
 	{
-		texts = [];
-
 		#if (discord_rpc && !hl)
 		Discord.changePresence('In the control menu', 'Probably trying to adjust their controls');
 		#end
