13f8b90e6c83d0e47186832df88142eab6f1663e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-11 08:54:53 -0500

    chore: option menu options stuff + Im gonna have to figure out custom keybinds

---
+++ source/menus/options/ControlsMenu.hx
@@ -0,0 +1,14 @@
+package menus.options;
+
+class ControlsMenu extends FlxState
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
--- source/menus/options/OptionsMenuMain.hx
+++ source/menus/options/OptionsMenuMain.hx
@@ -1,7 +1,24 @@
 package menus.options;
 
+import flixel.util.typeLimit.NextState;
+
 class OptionsMenuMain extends FlxState
 {
+	public static var optionMenuNames:Array<String> = [];
+	public static var optionMenuDestinations:Map<String, NextState> = [];
+
+	public static function newOptionMenu(name:String, destination:NextState)
+	{
+		optionMenuNames.push(name);
+		optionMenuDestinations.set(name, destination);
+	}
+
+	override public function new()
+	{
+		super();
+		newOptionMenu('Controls Menu', () -> new ControlsMenu());
+	}
+
 	override function create()
 	{
 		super.create();
