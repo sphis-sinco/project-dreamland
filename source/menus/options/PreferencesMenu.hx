4ef387a514728e9627a02d6998cfddf85470969d
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-12 18:08:36 -0500

    chore: reading documentation and fixing MORE ISSUES

--- source/menus/options/PreferencesMenu.hx
+++ source/menus/options/PreferencesMenu.hx
@@ -42,8 +42,12 @@ class PreferencesMenu extends TextSelecting
 					lastSelection = 0;
 					previousCameraPosition = null;
 
+					Global.NEW_HIGHSCORE = false;
 					Global.HIGHSCORE = 0;
+					PlayState.HIGHSCORE = false;
+					PlayState.SCORE = 0;
 					FlxG.save.erase();
+					FlxG.save.bind('dreamland', Application.current.meta.get('company'));
 					FlxG.resetGame();
 			}
 
