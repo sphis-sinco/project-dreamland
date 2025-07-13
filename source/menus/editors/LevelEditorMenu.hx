91cb3b6711c01d1b43279c6aec91bb0dc0a06d0e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-13 16:42:36 -0500

    for some reason the menu isnt workin?g

--- source/menus/editors/LevelEditorMenu.hx
+++ source/menus/editors/LevelEditorMenu.hx
@@ -1,3 +1,32 @@
 package menus.editors;
 
-class LevelEditorMenu extends FlxState {}
+import flixel.addons.ui.FlxUITabMenu;
+
+class LevelEditorMenu extends FlxState
+{
+	public var UI_BOX:FlxUITabMenu;
+
+	override function create()
+	{
+		super.create();
+
+		trace('Level Editor');
+
+		var tabs = [
+			{name: "Misc", label: 'Misc'},
+			{name: "Assets", label: 'Assets'},
+			{name: "Settings", label: 'Settings'}
+		];
+
+		UI_BOX = new FlxUITabMenu(null, tabs, true);
+
+		UI_BOX.resize(FlxG.width, FlxG.height - 128);
+		UI_BOX.screenCenter();
+		add(UI_BOX);
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
